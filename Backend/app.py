2a6b6e94e25cf0672e049ac96e7f496b2163854d
devnathanie <natichuchu2018@gmail.com>
2024-09-13 18:07:03 -0400

    Updated app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,18 @@
+from flask_cors import CORS
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import flask_sqlalchemy
+from dotenv import load_dotenv
+import os
 
+
+app = Flask(__name__)
+
+username = os.getenv("USERNAME")
+password = os.getenv("Password")
+database = os.getenv("DB")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@localhost:3000/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+CORS(app)
